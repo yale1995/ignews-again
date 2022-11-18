41e4aa0c43b2fa3d2c01a1ee1b50db3dcedc3c73
Yale Araujo <yale850@gmail.com>
2022-11-17 22:31:16 -0300

    feat: preview page and styles

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,88 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { RichText } from "prismic-dom";
+import { prismic } from "../../../services/prismic";
+
+import Head from "next/head";
+import styles from "../post.module.scss";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updated_at: string;
+  };
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const { data: session } = useSession();
+  const title = `${post.title} | Ignews`;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      router.push(`/posts/${post.slug}`);
+    }
+  }, [session]);
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updated_at}</time>
+          <div
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const response = await prismic.getByUID("publication", String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updated_at: new Date(response.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
